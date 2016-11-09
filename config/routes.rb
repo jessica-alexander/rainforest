58019f308961d6fd171362ce8643f2e8b2e254f2
Jessica <jhai_alex@hotmail.com>
2016-11-09 18:58:17 -0500

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
 resources :products
+resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
