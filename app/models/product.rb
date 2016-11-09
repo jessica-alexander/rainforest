7073c07994e4c21290fe83b7ed7e180b3a433d13
Jessica <jhai_alex@hotmail.com>
2016-11-09 18:26:52 -0500

    added url field, added html to show and index files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
 
-validates :description, :name, presence: true
+validates :description, :name, :url, presence: true
 validates :price_in_cents, numericality: {only_decimal: true}
 end
