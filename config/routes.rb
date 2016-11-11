96ff4418b1e8715f1f570e1161cbdbd5216d13ad
Jessica <jhai_alex@hotmail.com>
2016-11-11 06:54:45 -0500

    added reviews controller, added before action, created load_product method, update show page to include review form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
-  get 'sessions/create'
 
-  get 'sessions/destroy'
 
-  get 'users/new'
-
-  get 'users/create'
-
-resources :products
 resources :users, only: [:new, :create]
 resources :sessions, only: [:new, :create, :destroy]
-
+resources :products do
+  resources :reviews, only: [:show, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
