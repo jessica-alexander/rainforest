60b4f8b3efec68b4588fb03d415dbe0bcbda7cf1
Jessica <jhai_alex@hotmail.com>
2016-11-13 11:44:09 -0500

    Adding users names

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
 
   has_many :reviews
+  has_many :users, through: :reviews
 
 validates :description, :name, :url, presence: true
 validates :price_in_cents, numericality: {only_decimal: true}
